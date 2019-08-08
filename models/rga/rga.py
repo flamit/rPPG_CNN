90238d57d0ef08c05ea50df46bda28664936a213
HasnainRaz <hasnain3257@gmail.com>
2019-08-09 01:53:09 +0500

    RGA-Channel

--- models/rga/rga.py
+++ models/rga/rga.py
@@ -32,7 +32,7 @@ class RGAS(nn.Module):
         affinity = torch.zeros(size=[b, h * w, h * w])
         # Calculate affinity as convolution over each batch for faster computation:
         for batch in range(b):
-            kernel = phi[batch].permute(0, 1).view(h * w, c // self.s, 1, 1)
+            kernel = phi[batch].permute(1, 0).view(h * w, c // self.s, 1, 1)
             r = F.conv2d(theta[batch].view(1, c // self.s, h, w), kernel).view(h * w, h * w)
             affinity[batch] = r
         # Take out affinity row wise and column wise and reshape to image dimensions.
@@ -51,8 +51,39 @@ class RGAS(nn.Module):
         return x * a
 
 
-# TODO Channel attention part, then combine and add to resnet:
+# TODO Code cleanup and verification, add to resnet.
 class RGAC(nn.Module):
     def __init__(self, inplanes, h, w, s, affinity_out, s2):
         super(RGAC, self).__init__()
-        pass
+        self.s = s
+        self.conv1 = nn.Conv2d(h * w, h * w // s, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(h * w // s)
+        self.conv2 = nn.Conv2d(h * w, h * w // s, kernel_size=1)
+        self.bn2 = nn.BatchNorm2d(h * w // s)
+        self.relu = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(inplanes, inplanes, kernel_size=1)
+        self.bn3 = nn.BatchNorm2d(inplanes)
+        self.conv4 = nn.Conv2d(1 + 2 * inplanes, 1, kernel_size=1)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x_flat = x.permute(0, 2, 3, 1).view(b, h * w, -1, 1)
+        theta = self.relu(self.bn1(self.conv1(x_flat)))  # B x H*W x C / s
+        phi = self.relu(self.bn2(self.conv2(x_flat)))  # B x H*W x C / s
+
+        affinity = torch.zeros(size=[b, c, c])
+        for batch in range(b):
+            # kernel: outplanes, inplanes, 1, 1
+            kernel = phi[batch].permute(1, 0, 2).contiguous().view(c, -1, 1, 1)
+            r = F.conv2d(theta[batch].view(1, -1, c, 1), kernel).view(c, c)
+            affinity[batch] = r
+        # Take out affinity row wise and column wise and reshape to image dimensions.
+        affinity_a = affinity.view(b, -1, c, c).permute(0, 2, 1, 3)
+        affinity_b = affinity.permute(0, 2, 1).view(b, -1, c, c).permute(0, 2, 1, 3)
+
+        x_embed = self.relu(self.bn3(self.conv3(x))).mean(dim=3, keepdim=True).mean(dim=2, keepdim=True).permute(0, 2, 3, 1)  # Mean pool over space
+        y = torch.cat([x_embed, affinity_a, affinity_b], dim=1)
+        a = self.sig(self.conv4(y)).permute(0, 3, 1, 2)
+        return x * a
